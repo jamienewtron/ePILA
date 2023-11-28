7bc5effc09eb162df701fd5b7fccbd5cc3bfccb3
jamienewtron <140039543+jamienewtron@users.noreply.github.com>
2023-11-28 23:32:23 +0800

    Update announcement.dart

--- lib/announcement.dart
+++ lib/announcement.dart
@@ -1,37 +1,56 @@
 import 'package:flutter/material.dart';
 
 class Announcement extends StatelessWidget {
-  const Announcement({super.key});
+  const Announcement({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            const SizedBox(
-              height: 200,
+      body: Stack(
+        children: <Widget>[
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 200),
+                Text(
+                  'ANNOUNCEMENTS',
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 0, 35, 87),
+                    fontSize: 26,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: Image.asset('assets/sample1.png'),
+                ),
+                SizedBox(height: 15),
+              ],
             ),
-            Text(
-              'ANNOUNCEMENTS',
-              style: TextStyle(
-                color: const Color.fromARGB(255, 0, 35, 87),
-                fontSize: 26,
-                fontWeight: FontWeight.w900,
+          ),
+          Positioned(
+            top: 10,
+            right: 10,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: const Color.fromARGB(255, 0, 35, 87),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: Icon(
+                  Icons.close,
+                  color: Colors.white,
+                ),
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: Image.asset('assets/sample1.png'),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
